86dde1746026e7f5431ae5bbf9003c0dcc42ef41
jordyn hollingsworth <jordyn@jordyns-macbook-air.local>
2018-12-06 20:50:41 -0800

    Initial Commit

---
+++ TravelPalApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelPalApp
+//
+//  Created by jordyn hollingsworth on 12/6/18.
+//  Copyright © 2018 jordyn hollingsworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

df8cf45fb6c372a3fbe51afdac91c223772734ce
DMSIMAC02 <DMSIMAC02@dhcp-lib-78-173.sfsu.edu>
2018-12-13 14:34:14 -0800

    Added struct for managing  global settings variables

---
+++ TravelPalApp/GlobalSettings.swift
@@ -0,0 +1,20 @@
+//
+//  globalSettings.swift
+//  TravelPalApp
+//
+//  Created by DMSIMAC02 on 12/13/18.
+//  Copyright © 2018 jordyn hollingsworth. All rights reserved.
+//
+
+import Foundation
+
+public struct GlobalSettings{
+    static var language:String = "english"
+    static var route:String = "SF"
+    static var lookingForToilet:Bool = false
+}
+
+
+
+
+
